6629b8fd8d5ba44670b8b21615efbd30e7322a05
saikrishna899 <anhsirk19@gmail.com>
2025-04-18 16:06:20 +0530

    add code

--- Arrays/moveZerosEnd.java
+++ Arrays/moveZerosEnd.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+//The problem is to move all the 0’s to the end of the array
+
 public class moveZerosEnd {
     public static void main(String[] args) {
         int[] nums = {0, 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
