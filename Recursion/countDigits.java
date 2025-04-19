b102b329ff9116fff699581055a4e8d5b076e30b
saikrishna899 <anhsirk19@gmail.com>
2025-04-19 20:01:17 +0530

    added code

---
+++ Recursion/countDigits.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+
+public class countDigits {
+    public static void main(String[] args) {
+        System.out.println(countDigits(12309010));
+    }
+
+    //Time Complexity & space = O(d)
+    //or O(log₁₀(n)), since d ≈ log₁₀(n)
+    public static int countDigits(int n) {
+        // add your code here
+        if(n/10 == 0){
+            return 1;
+        }
+        return 1 + countDigits(n/10);
+    }
+}
