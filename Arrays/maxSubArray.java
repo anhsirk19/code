5ee47ce6daa35b9591c68e207202a0c34b0980e7
saikrishna899 <anhsirk19@gmail.com>
2025-04-21 11:51:23 +0530

    added code to print the max subarr range

--- Arrays/maxSubArray.java
+++ Arrays/maxSubArray.java
@@ -1,7 +1,7 @@
 public class maxSubArray {
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(answer2(arr));
+        System.out.println(maxSubarraySum(arr));
     }
     //brute 
     //tc is o(n^3)
@@ -41,5 +41,32 @@ public class maxSubArray {
             max_so_far = Math.max(curr_sum, max_so_far);
         }
         return max_so_far;
+    }
+
+    //to print the range also
+    static int maxSubarraySum(int[] arr) {
+        int curr_sum = arr[0];
+        int max_sum = arr[0];
+        int start = 0;
+        int end = 0;
+        int fStart = 0;
+        int fEnd = 0;
+        
+        for(int i = 1; i < arr.length ; i++){
+            if(curr_sum + arr[i] > arr[i]){
+                curr_sum = curr_sum + arr[i];
+                end = i;
+            }else{
+                curr_sum = arr[i];
+                start = i;
+            }
+            if(max_sum < curr_sum){
+                fStart = start;
+                fEnd = end;
+                max_sum = curr_sum;
+            }
+        }
+        System.out.println(fStart + " -> " + fEnd);
+        return max_sum;
     }
 }
