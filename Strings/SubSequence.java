47b264ba8554c338a7a2be757896eec7188f6130
saikrishna899 <anhsirk19@gmail.com>
2025-04-28 12:07:25 +0530

    added rec approach

--- Strings/SubSequence.java
+++ Strings/SubSequence.java
@@ -8,8 +8,28 @@ public class SubSequence {
         String str1 = "AXY";
         String str2 = "ADXCPYI";
         System.out.println(solution(str1, str2));
+        System.out.println(isSub(str1, str2, 3, 7));
     }
-    //tc is o(n)
+    //tc is o(m), m = s2.len as s1 < s2
+    //sc is o(m), atmost m stack calls
+    
+    static boolean isSub(String s1, String s2, int n, int m){
+        if(n > m){
+            return false;
+        }
+        if(n == 0){
+            return true;
+        }
+        if(m == 0){
+            return false;
+        }
+        if(s1.charAt(n-1) == s2.charAt(m-1)){
+            return isSub(s1, s2, n-1, m-1);
+        }else{
+            return isSub(s1, s2, n, m-1);
+        }
+    }
+    //tc is o(m), m = s2.len as s1 < s2
     //sc is o(1)
     static boolean solution(String s1, String s2){
         if(s1.length() > s2.length()){
