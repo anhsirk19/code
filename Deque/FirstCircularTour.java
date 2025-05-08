0c2ad4e624ec7814148e1f84f8a9f184809d6cfb
saikrishna899 <anhsirk19@gmail.com>
2025-05-08 22:41:32 +0530

    added code

---
+++ Deque/FirstCircularTour.java
@@ -0,0 +1,29 @@
+package Deque;
+
+public class FirstCircularTour {
+    public static void main(String[] args) {
+        
+    }
+
+    //tc is o(n)
+    //sc is o(1)
+    public int startStation(int[] gas, int[] cost) {
+        // Your code here
+        int totalGas = 0;
+        int totalCost = 0;
+        
+        int start = 0,  currGas = 0;
+        for(int i = 0 ; i < gas.length ; i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
+            currGas = currGas + gas[i] - cost[i];
+            if(currGas < 0){
+                start = i+1;
+                currGas = 0;
+            }
+        }
+        return (totalGas < totalCost) ? -1 : start;
+    }
+
+
+}
